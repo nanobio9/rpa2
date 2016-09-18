ea8b30ac32edb9b1fd4042534a43ca7a78b18f8d
LIANG Kuo Kan <kkliang@LIANG-Kuo-Kanteki-MacBook-Air.local>
2016-09-18 23:08:31 +0800

    final

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ iv <- NULL
+ set <- function( y ) {
+ 	mx <<- y
+ 	iv <<- NULL
+ }
+ get <- function() mx
+ setiv <- function( inverse ) iv <<- inverse
+ getiv <- function() iv
+ list( set = set, get = get, setiv = setiv, getiv = getiv )
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+ ## Return a matrix that is the inverse of 'x'
+ iv <- x$getiv()
+ if( !is.null( iv ) ) {
+ 	message("Getting the cached inverse...")
+ 	return( iv )
+ }
+ mx <- x$get()
+ iv <- solve( mx, ... )
+ x$setiv( iv )
+ iv
 }
